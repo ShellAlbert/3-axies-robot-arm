39e6031134b57d12814186e7c984bbd86d5f13d3
zhangshaoyan <1811543668@qq.com>
2020-09-04 10:22:42 +0800

    add profile position mode random relative position test.

---
+++ test_app/ppm_random_relative_position/ppm_relative_test.c
@@ -0,0 +1,35 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <string.h>
+#define FIFO_NAME "/tmp/servo.0"
+int g_bExitFlag=0;
+int main(int argc,char **argv)
+{
+    int fd;
+    int a=-2000,b=2000;
+    fd=open(FIFO_NAME,O_WRONLY);
+    if(fd<0)
+    {
+        fprintf(stderr,"failed to open fifo %s\n",FIFO_NAME);
+        return -1;
+    }
+    srand(time(0));
+    while(!g_bExitFlag)
+    {
+        char buffer[256];
+        int pos=rand()%(b-a+1)+a;
+        int len;
+        //Profile Position Mode - Relative Position.
+        sprintf(buffer,"ppm-rel=%d\n",pos);
+        len=strlen(buffer);
+        write(fd,(void*)&len,sizeof(len));
+        write(fd,(void*)buffer,len);
+        printf("%d:%s\n",len,buffer);
+        sleep(5);
+    }
+    close(fd);
+    return 0;
+}
